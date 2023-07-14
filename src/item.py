36a63c3fd8fbbf4a450f4eda926d6a659ff4191e
Sergey <smirsand@mail.ru>
2023-07-14 22:03:39 +0300

    add cod item.py

--- src/item.py
+++ src/item.py
@@ -1,3 +1,4 @@
+import json
 import os
 from abc import ABC, abstractmethod
 import requests
@@ -12,11 +13,14 @@ class API(ABC):
 
 
 class HeadHunterAPI(API):
-    def get_vacancies(self, search_query):
+    def get_vacancies(self, search_query, location=None, page=0, per_page=100, only_with_salary=True):
         url = "https://api.hh.ru/vacancies"
         params = {
-            "text": search_query,
-            "per_page": 10
+            "text": f"NAME:{search_query}",
+            "area": location,
+            "page": str(page),
+            "per_page": str(per_page),
+            "only_with_salary": str(only_with_salary)
         }
 
         response = requests.get(url, params=params)
@@ -90,22 +94,70 @@ class FileManager(ABC):
         pass
 
 
+class JsonFileManager(FileManager):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def add_vacancy(self, vacancy):
+        with open(self.filename, 'a', encoding='utf-8') as file:
+            vacancy_dict = {
+                'title': vacancy.title,
+                'link': vacancy.link,
+                'salary': vacancy.salary,
+                'description': vacancy.description
+            }
+            json.dump(vacancy_dict, file)
+            file.write('\n')
+
+    def get_vacancies(self, criteria):
+        vacancies = []
+        with open(self.filename, 'r', encoding='utf-8') as file:
+            for line in file:
+                vacancy_dict = json.loads(line)
+                if criteria in vacancy_dict['title']:
+                    title = vacancy_dict['title']
+                    link = vacancy_dict['link']
+                    salary = vacancy_dict['salary']
+                    description = vacancy_dict['description']
+
+                    vacancy = Vacancy(title, link, salary, description)
+                    vacancies.append(vacancy)
+
+        return vacancies
+
+    def delete_vacancies(self):
+        open(self.filename, 'w', encoding='utf-8').close()
+
+
 def user_interaction():
     search_query = input("Введите поисковый запрос: ")
+    location = input("Регион: ")
 
     headhunter_api = HeadHunterAPI()
     superjob_api = SuperJobAPI()
 
-    vacancies_hh = headhunter_api.get_vacancies(search_query)
+    vacancies_hh = headhunter_api.get_vacancies(search_query, location)
     vacancies_sj = superjob_api.get_vacancies(search_query)
+
+    json_file_manager = JsonFileManager("vacancies.json")
 
     print("\nРезультаты поиска из HeadHunter:")
     for vacancy in vacancies_hh:
+        json_file_manager.add_vacancy(vacancy)
         print(vacancy)
         print()
 
     print("Результаты поиска из SuperJob:")
     for vacancy in vacancies_sj:
+        json_file_manager.add_vacancy(vacancy)
+        print(vacancy)
+        print()
+
+    criteria = input("Введите критерий для поиска из сохраненных вакансий: ")
+    saved_vacancies = json_file_manager.get_vacancies(criteria)
+
+    print(f"\nРезультаты поиска из сохраненных вакансий по критерию '{criteria}':")
+    for vacancy in saved_vacancies:
         print(vacancy)
         print()
 
